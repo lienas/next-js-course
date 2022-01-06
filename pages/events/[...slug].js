83c64c779f2e8ff8532acc9019a26f37e0a2fe4e
thomas <t.lucas@lienas.de>
2022-01-06 20:25:01 +0100

    Abschitt 4 file-based-routing

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,8 +1,72 @@
-function FiltersEventsPage(){
-    return(
-        <div>
-            <h1>Filterd Events</h1>
-        </div>
+import {useRouter} from "next/router";
+
+import {getFilteredEvents} from "../../dummy-data";
+import EventList from "../../components/events/EventList";
+import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
+
+function FiltersEventsPage() {
+
+    const router = useRouter();
+    const filterData = router.query.slug;
+
+    if (!filterData) {
+        return <p className='center'>Loading ... </p>
+    }
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if (
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth < 1 ||
+        numMonth > 12
+    ) {
+        return (
+            <>
+                <ErrorAlert>
+                    <p>invalid Filter !!!!</p>
+                </ErrorAlert>
+                <div className='center'>
+                    <Button link="/events">Show all Events</Button>
+                </div>
+            </>
+        )
+    }
+
+    const events = getFilteredEvents({
+        year: numYear,
+        month: numMonth
+    });
+
+    if (!events || events.length === 0) {
+        return (
+            <>
+                <ErrorAlert>
+                    <p className='center'>No Events found !!!</p><
+                    /ErrorAlert>
+                <div className='center'>
+                    <Button link="/events">Show all Events</Button>
+                </div>
+
+            </>
+        )
+    }
+
+    const date = new Date(numYear, numMonth - 1);
+
+    return (
+        <>
+            <ResultsTitle date={date}/>
+            <EventList items={events}/>
+        </>
     )
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,24 @@
 import {getAllEvents} from "../../dummy-data";
+import {useRouter} from "next/router";
+
+import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
+
+    const events = getAllEvents();
+    const router = useRouter();
+
+    const findEventsHandler = (year, month) => {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath)
+    }
+
     return (
-        <div>
-            <h1>Events Page</h1>
-            {getAllEvents().map(event =>
-            <h2>{event.title}</h2>
-            ) }
-        </div>
+        <>
+            <EventsSearch onSearch={findEventsHandler}/>
+            <EventList items={events}/>
+        </>
     )
 }
 
